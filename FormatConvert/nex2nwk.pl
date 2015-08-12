6ada7997f9c9a72bedf26cb9013e147a342aacfa
NoelDylan <nevrfa@gmail.com>
2015-08-12 16:39:32 +0800

    Simple script to convert nexus trees into newick trees.
    BioPerl is required.

---
+++ FormatConvert/nex2nwk.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+# By Noel Yang.
+# Simple script to convert nexus trees into newick trees. BioPerl is required.
+
+use strict;
+use Bio::TreeIO;
+
+my $usage = "script.pl INFILE OUTFILE\n";
+my $infile = shift or die $usage;
+my $outfile = shift or die $usage;
+
+my ($filein,$fileout) = @ARGV;
+my ($format,$oformat) = qw(nexus newick);
+my $in = Bio::TreeIO->new(-file => $infile, -format => $format);
+my $out= Bio::TreeIO->new(-format => $oformat, -file => ">$outfile");
+
+while( my $t = $in->next_tree ) {
+	$out->write_tree($t);
+}
